bd6d7932cd36900eac9b2c16308bf9a18770f12a
Edrin Avdullahu <edrin.business@gmail.com>
2024-12-02 11:22:28 +0100

    second commit

--- HolidayMaker/Program.cs
+++ HolidayMaker/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HolidayMaker;
+
+class Program
+{
+
+  static void Main(string[] args)
+  {
+    // anslut till databasen
+    Database database = new();
+    // hämta anslutningen (db) att göra queries med
+    var db = database.Connection();
+    // skapa actions och skicka in anslutningen, så att vi kan köra queries till databasen där
+    var actions = new Actions(db);
+    // skapa en meny och skicka in våra actions i den, så vi kan anropa dem
+    new Menu(actions);
+  }
+}
--- HolidayMaker/obj/Debug/net8.0/HolidayMaker.AssemblyInfo.cs
+++ HolidayMaker/obj/Debug/net8.0/HolidayMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HolidayMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+418ce106f6f13a56f7099a0ec777d8f4bea0036c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a7c76f7507ab2d44e89a05348f8820978e7daa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HolidayMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HolidayMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
